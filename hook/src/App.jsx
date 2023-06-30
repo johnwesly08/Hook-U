5d9a8542317501d9bd72b40c7df0f6ef02175221
johnwesly08 <johnwesleywork08@gmail.com>
2023-06-30 07:39:10 +0530

    Login page Design for form completed.

--- hook/src/App.jsx
+++ hook/src/App.jsx
@@ -1,18 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Fragment } from 'react'
+import { Login } from './login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Fragment>
     <div className="login">
-      
+      <Login/>
     </div>
-    </>
+    </Fragment>
   )
 }
 
-export default App
+export default App;
---
+++ hook/src/login.jsx
@@ -0,0 +1,17 @@
+import React,{Fragment} from 'react'
+
+
+export function Login() {
+    return (
+            <div className="Login">
+                <form method="post/get">
+                    <h1>Login</h1>
+                    <label  for="username">Username</label><input type="text" id="uname" name="uname"></input>
+                    <label  for="password">Password</label><input type="password" id="pwd" name="pwd"></input>
+                    <input type="submit" id="button" name="button"></input>
+                </form>
+                <p id="paragraph">Not have an account?<a href="#">Sign Up Here</a></p>
+            </div>
+    )
+
+};
